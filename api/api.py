fbc820fc7caea2568d5269f303dad5578f8a566d
Rohit Jain <rohit@sendinblue.com>
2019-04-18 13:58:24 +0530

    add getresponse api call

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify,Response,request
 from flask_cors import CORS
 import os
 import requests
+import json
 
 application = Flask(__name__)
 cors = CORS(application, resources={r"*": {"origins": "http://www.getmassivesales.com"}})
@@ -11,6 +12,10 @@ WEBINARJAM_API_KEY = os.environ.get("WEBINARJAM_API_KEY", '')
 WEBINARJAM_WEBINAR_ID = os.environ.get("WEBINARJAM_WEBINAR_ID", '')
 WEBINARJAM_GET_WEBINAR_SCHEDULE = "https://webinarjam.genndi.com/api/everwebinar/webinar"
 WEBINARJAM_REGISTER_WEBINAR = "https://webinarjam.genndi.com/api/everwebinar/register"
+
+GET_RESPONSE_API_KEY = os.environ.get("GET_RESPONSE_API_KEY", '')
+GET_RESPONSE_LIST_ID = os.environ.get("GET_RESPONSE_LIST_ID", '')
+GET_RESPONSE_ADD_CONTACT = "https://api.getresponse.com/v3/contacts"
 
 @application.route('/schedule/webinars/<webinar_id>/', methods=['GET'])
 def get_webinar_schedule(webinar_id):
@@ -46,6 +51,8 @@ def register_webinar(webinar_id,schedule):
 	if not webinar_exist:
 		return Response("Something went wrong", status=500, mimetype='application/json')
 
+	add_subscriber_getResponse(request.get_json())
+
 	return jsonify(webinar_response)
 
 def build_webinar_register_response(webinar_id,schedule,user_data):
@@ -73,6 +80,36 @@ def build_webinar_register_response(webinar_id,schedule,user_data):
 	except KeyError:
 		return {},False
 
+def add_subscriber_getResponse(user_data):
+
+	getResponseRequestBody = {
+        "name": user_data.get("first_name",""),
+        "email": user_data.get("email",''),
+        "campaign": {
+            "campaignId": GET_RESPONSE_LIST_ID
+        },
+        "customFieldValues": [
+            {
+                "customFieldId": "VVzZGE",
+                "value": [user_data.get("category",'')]
+            },
+            {
+                "customFieldId": "YeBiy",
+                "value": [user_data.get("phone",'')]
+            }
+        ]
+	}
+
+	headers = {
+	   'X-Auth-Token': 'api-key ' + GET_RESPONSE_API_KEY,
+	   'Content-Type':"application/json"
+    }
+	req = requests.post(GET_RESPONSE_ADD_CONTACT,data=json.dumps(getResponseRequestBody),headers=headers)
+
+	if not req.status_code == 202:
+		print(headers)
+		print(getResponseRequestBody)
+		print(req.content)
 
 if __name__ == '__main__':
 	application.run(debug=True,host = '0.0.0.0')
